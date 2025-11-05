33918302ea1914b98aa3ac624b9116cc06f3a118
KOLISETTY SREYA SRI <kolisettysreya25@gmail.com>
2025-11-05 11:48:11 +0530

    Update CART_API_URL to production endpoint

--- src/services/cartService.js
+++ src/services/cartService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Use the correct base URL for cart operations
-const CART_API_URL = "http://localhost:30083/back1/api/cart";
+const CART_API_URL = "http://ec2-13-221-26-213.compute-1.amazonaws.com:30083/back1/api/cart";
 
 export const addToCart = async (userId, productId) => {
   return await axios.post(`${CART_API_URL}/add`, {
