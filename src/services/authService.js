e7d69eebe7d2f8e3c838f7d09da5f2b5caac75fb
KOLISETTY SREYA SRI <kolisettysreya25@gmail.com>
2025-11-05 11:48:33 +0530

    Update API_URL to point to production server

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:30083/back1/auth";
+const API_URL = "http://ec2-13-221-26-213.compute-1.amazonaws.com:30083/back1/auth";
 
 export const login = async (username, password) => {
   const response = await axios.post(`${API_URL}/login`, { username, password });
